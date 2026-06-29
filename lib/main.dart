648a76133f6f61354ea8a83b5a9aa2e28e07d424
squallm3 <ar.mmarra@gmail.com>
2026-06-29 10:41:43 -0300

    alpha v2.9 feat: 73 enable Firestore offline cache for better performance

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'app.dart';
-
+ 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+    cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+  );
   runApp(const ProviderScope(child: HaikuGnosticoApp()));
 }
+ 
